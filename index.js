013bf03a08c393803b15bc87dd88e6c4ca4f8cb6
waqaryounis7564 <waqaryounis7564@gmail.com>
2019-02-17 14:50:56 +0500

    testing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   winston.info(`listening to port ${port}...`)
 );
